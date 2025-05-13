9535aa72301f2c630ac3bc329ba319055deee51e
kaushithamsilva <kaushithamsilva@gmail.com>
2025-05-14 00:44:43 +0530

    office adapter

--- CLIP_Explorations/train_adapter.py
+++ CLIP_Explorations/train_adapter.py
@@ -152,8 +152,15 @@ def main():
     model.to(device)
     model.eval()
 
-    classes_str = "Alarm Clock, Backpack, Batteries, Bed, Bike, Bottle, Bucket, Calculator, Calendar, Candles, Chair, Clipboards, Computer, Couch, Curtains, Desk Lamp, Drill, Eraser, Exit Sign, Fan,File Cabinet, Flipflops, Flowers, Folder, Fork, Glasses, Hammer, Helmet, Kettle, Keyboard,Knives, Lamp Shade, Laptop, Marker, Monitor, Mop, Mouse, Mug, Notebook, Oven, Pan,Paper Clip, Pen, Pencil, Postit Notes, Printer, Push Pin, Radio, Refrigerator, ruler,Scissors, Screwdriver, Shelf, Sink, Sneakers, Soda, Speaker, Spoon, Table, Telephone,Toothbrush, Toys, Trash Can, TV, Webcam"
-    classes = classes_str.split(",")
+    classes = [
+        "Alarm Clock", "Backpack", "Batteries", "Bed", "Bike", "Bottle", "Bucket", "Calculator", "Calendar", "Candles",
+        "Chair", "Clipboards", "Computer", "Couch", "Curtains", "Desk Lamp", "Drill", "Eraser", "Exit Sign", "Fan",
+        "File Cabinet", "Flipflops", "Flowers", "Folder", "Fork", "Glasses", "Hammer", "Helmet", "Kettle", "Keyboard",
+        "Knives", "Lamp Shade", "Laptop", "Marker", "Monitor", "Mop", "Mouse", "Mug", "Notebook", "Oven", "Pan",
+        "Paper Clip", "Pen", "Pencil", "Postit Notes", "Printer", "Push Pin", "Radio", "Refrigerator", "Ruler",
+        "Scissors", "Screwdriver", "Shelf", "Sink", "Sneakers", "Soda", "Speaker", "Spoon", "Table", "Telephone",
+        "Toothbrush", "Toys", "Trash Can", "TV", "Webcam"
+    ]
     classes = [c.lower().strip() for c in classes]
     print(f"Number of classes: {len(classes)}")
 
