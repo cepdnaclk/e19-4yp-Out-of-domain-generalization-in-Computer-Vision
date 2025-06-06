d43cbbc29df78ecf863f16e329dab4347728860a
kaushithamsilva <kaushithamsilva@gmail.com>
2025-06-07 01:58:47 +0530

    start from scratch, no init prompts

--- BiomedCLIP_EA_Experiments/prompt_optimizer.py
+++ BiomedCLIP_EA_Experiments/prompt_optimizer.py
@@ -125,8 +125,8 @@ def main():
     # """
 
     # Optimization loop
-    initial_prompts = util.load_initial_prompts("initial_prompts.txt")
-    pq = util.PriorityQueue(max_capacity=1000, initial=initial_prompts)
+    # initial_prompts = util.load_initial_prompts("initial_prompts.txt")
+    pq = util.PriorityQueue(max_capacity=1000)
     prompt_content = ""
 
     for j in range(2000):
