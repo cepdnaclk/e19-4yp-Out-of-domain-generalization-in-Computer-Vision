95a269bb3ccabfd38b4aecd8013ff435b1635b03
kaushithamsilva <kaushithamsilva@gmail.com>
2025-06-07 01:38:04 +0530

    removed system prompt

--- LLMs/ollama_test.py
+++ LLMs/ollama_test.py
@@ -2,7 +2,6 @@ import ollama
 
 # Define your conversation messages
 messages = [
-    {'role': 'system', 'content': 'You are a helpful assistant.'},
     {'role': 'user', 'content': 'Why is the sky blue?'},
 ]
 
