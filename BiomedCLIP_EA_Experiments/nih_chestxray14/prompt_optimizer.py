4d1495bd3fc82e39a4f5dc81928b6f59e373ffba
Mansitha <iammansitha@gmail.com>
2025-07-06 01:15:19 +0530

    changed exp name

--- BiomedCLIP_EA_Experiments/nih_chestxray14/prompt_optimizer.py
+++ BiomedCLIP_EA_Experiments/nih_chestxray14/prompt_optimizer.py
@@ -74,7 +74,7 @@ def get_prompt_template(iteration_num: int, prompt_content: str, generate_n: int
 
 def main():
     # Name the experiment we are currently running
-    experiment_name = "NIH_Train_Experiments-01_Strategy_500Iterations_BCE"
+    experiment_name = "NIH_Train_Experiments-02_Strategy_500Iterations_BCE_After_Fix_Normal_Condition"
     print(f"Running {experiment_name}...")
 
     # Create experiment results directory
