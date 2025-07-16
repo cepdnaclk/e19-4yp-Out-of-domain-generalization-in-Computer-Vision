4bf6adc67739c4b41098b10a2ca3e1913ebd1aa1
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2025-07-16 22:39:45 +0530

    added pneumonia test on nih dataset

--- BiomedCLIP_EA_Experiments/nih_chestxray14/prompt_optimizer.py
+++ BiomedCLIP_EA_Experiments/nih_chestxray14/prompt_optimizer.py
@@ -74,7 +74,7 @@ def get_prompt_template(iteration_num: int, prompt_content: str, generate_n: int
 
 def main():
     # Name the experiment we are currently running
-    experiment_name = "NIH_Train_Experiments-02_Strategy_500Iterations_BCE_After_Fix_Normal_Condition"
+    experiment_name = "NIH_Train_Experiments-03_disease_pneumonia"
     print(f"Running {experiment_name}...")
 
     # Create experiment results directory
