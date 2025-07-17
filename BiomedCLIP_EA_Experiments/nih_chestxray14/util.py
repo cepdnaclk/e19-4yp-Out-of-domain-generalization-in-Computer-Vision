2f904abfd660ededc2e1a09551d4b5d4bbd69ad3
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2025-07-17 14:15:46 +0530

    Added experiment label Cardiomegaly

--- BiomedCLIP_EA_Experiments/nih_chestxray14/util.py
+++ BiomedCLIP_EA_Experiments/nih_chestxray14/util.py
@@ -81,7 +81,7 @@ PromptList = List[Tuple[PromptPair, float]]
 
 
 class NIHChestXRayDataset(Dataset):
-    def __init__(self, df, image_dir, preprocess, target_label="Pleural_Thickening"):
+    def __init__(self, df, image_dir, preprocess, target_label="Cardiomegaly"):
         self.df = df[df['Finding Labels'].apply(
             lambda x: target_label in x.split('|') or x == 'No Finding'
         )]
