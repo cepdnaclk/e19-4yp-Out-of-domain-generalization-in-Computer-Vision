6c53ddfd35f1ca501b6246e9067ae6775d2cf669
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2025-07-17 14:19:08 +0530

    Added experiment label Edema

--- BiomedCLIP_EA_Experiments/nih_chestxray14/prompt_optimizer.py
+++ BiomedCLIP_EA_Experiments/nih_chestxray14/prompt_optimizer.py
@@ -74,7 +74,7 @@ def get_prompt_template(iteration_num: int, prompt_content: str, generate_n: int
 
 def main():
     # Name the experiment we are currently running
-    experiment_name = "NIH_Train_Experiments-13_Emphysema"
+    experiment_name = "NIH_Train_Experiments-14_Edema"
     print(f"Running {experiment_name}...")
 
     # Create experiment results directory
