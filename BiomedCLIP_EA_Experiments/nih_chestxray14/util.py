8eb4eced420f3f88621809910f410a98d927c750
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2025-07-17 14:09:54 +0530

    Added experiment label Mass

--- BiomedCLIP_EA_Experiments/nih_chestxray14/util.py
+++ BiomedCLIP_EA_Experiments/nih_chestxray14/util.py
@@ -81,7 +81,7 @@ PromptList = List[Tuple[PromptPair, float]]
 
 
 class NIHChestXRayDataset(Dataset):
-    def __init__(self, df, image_dir, preprocess, target_label="Nodule"):
+    def __init__(self, df, image_dir, preprocess, target_label="Mass"):
         self.df = df[df['Finding Labels'].apply(
             lambda x: target_label in x.split('|') or x == 'No Finding'
         )]
