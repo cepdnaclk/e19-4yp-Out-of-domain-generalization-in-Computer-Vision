35a8bca237b39b81f728d8223e77a71df22a07b8
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2025-07-17 14:04:54 +0530

    Added experiment label Effusion

--- BiomedCLIP_EA_Experiments/nih_chestxray14/prompt_optimizer.py
+++ BiomedCLIP_EA_Experiments/nih_chestxray14/prompt_optimizer.py
@@ -74,7 +74,7 @@ def get_prompt_template(iteration_num: int, prompt_content: str, generate_n: int
 
 def main():
     # Name the experiment we are currently running
-    experiment_name = "NIH_Train_Experiments-04_disease_Infiltration"
+    experiment_name = "NIH_Train_Experiments-05_disease_Effusion"
     print(f"Running {experiment_name}...")
 
     # Create experiment results directory
