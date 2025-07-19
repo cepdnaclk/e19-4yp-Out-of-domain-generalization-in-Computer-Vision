12a466912769901d17106c8c6911abc9f1842b66
kaushithamsilva <kaushithamsilva@gmail.com>
2025-07-19 15:27:50 +0530

    knee point analysis

--- BiomedCLIP_EA_Experiments/camelyon17/stacking.py
+++ BiomedCLIP_EA_Experiments/camelyon17/stacking.py
@@ -52,7 +52,7 @@ def main():
         max_capacity=1000, filter_threshold=0.6, initial=initial_prompts)
     all_scores = [s for _, s in pq.get_best_n(len(pq))]
     recommended_n = util.KneePointAnalysis(all_scores).find_knee_point()
-    prompt_population = pq.get_best_n(len(pq))
+    prompt_population = pq.get_best_n(recommended_n)
     print(f"Selected {len(prompt_population)} prompts after knee analysis.")
 
     # Extract scores for normalization
