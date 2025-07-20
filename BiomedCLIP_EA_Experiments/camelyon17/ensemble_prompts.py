4b938cddbaa4267aa73b526666d5f70117e5bda7
kaushithamsilva <kaushithamsilva@gmail.com>
2025-07-20 17:43:26 +0530

    added crowding to the ensemble pipeline

--- BiomedCLIP_EA_Experiments/camelyon17/ensemble_prompts.py
+++ BiomedCLIP_EA_Experiments/camelyon17/ensemble_prompts.py
@@ -3,6 +3,7 @@ import util
 import torch
 import numpy as np
 import os
+import crowding
 
 
 def main():
@@ -27,13 +28,24 @@ def main():
     centers_labels = [torch.from_numpy(label) for label in centers_labels]
 
     # 3. load prompts
-    initial_prompts = util.load_initial_prompts(
-        "experiment_results/distinct_medical_concepts.txt")
+    initial_prompts = util.load_last_iteration_prompts(
+        "experiment_results/Experiment-61-strategy-inv-bce-gemma3-2shot_opt_pairs.txt")
 
     print(f"Initial prompts loaded: {len(initial_prompts)} prompts.")
 
-    pq = util.PriorityQueue(
-        max_capacity=1000, filter_threshold=0.6, initial=initial_prompts)
+    if not initial_prompts:
+        print("No initial prompts found. Exiting.")
+        return
+
+    # 4. Perform crowding pruning
+    # pq = util.PriorityQueue(
+    #     max_capacity=1000, filter_threshold=0.6, initial=initial_prompts)
+    pq = crowding.perform_crowding_pruning(
+        initial_prompts=initial_prompts,
+        number_of_prompts_to_group=30,
+        crowding_iterations=20,
+        max_retries=5
+    )
 
     # knee point analysis
     all_current_scores = [score for _, score in pq.get_best_n(
@@ -45,28 +57,25 @@ def main():
         f"Recommended number of prompts after knee analysis: {recommended_n}")
 
     prompts_population = pq.get_best_n(recommended_n)
+    print(f"Using {len(prompts_population)} prompts for evaluation.")
 
-    for i in range(0, len(prompts_population), 1):
-        prompts = prompts_population[0:i + 1]
-        print(f"Using {len(prompts)} prompts for evaluation.")
+    # Run the evaluation
+    for i, _ in enumerate(centers_features):
+        print(f"Evaluating center {i}...")
+        results = util.evaluate_prompt_list(
+            prompts_population,
+            centers_features[i],
+            centers_labels[i],
+            model,
+            tokenizer,
+            unweighted=False
+        )
 
-        # Run the evaluation
-        for i, _ in enumerate(centers_features):
-            print(f"Evaluating center {i}...")
-            results = util.evaluate_prompt_list(
-                prompts,
-                centers_features[i],
-                centers_labels[i],
-                model,
-                tokenizer,
-                unweighted=False
-            )
-
-            print("\n--- Ensemble Evaluation Results ---")
-            print(f"Accuracy: {results['accuracy']:.4f}")
-            print(f"AUC: {results['auc']:.4f}")
-            print("Confusion Matrix:\n", results['cm'])
-            print("Classification Report:\n", results['report'])
+        print("\n--- Ensemble Evaluation Results ---")
+        print(f"Accuracy: {results['accuracy']:.4f}")
+        print(f"AUC: {results['auc']:.4f}")
+        print("Confusion Matrix:\n", results['cm'])
+        print("Classification Report:\n", results['report'])
 
 
 if __name__ == "__main__":
