b97576f8cdb0918477e90670594e198598b92f3a
kaushithamsilva <kaushithamsilva@gmail.com>
2025-07-20 22:19:27 +0530

    no knee point analysis

--- BiomedCLIP_EA_Experiments/camelyon17/ensemble_prompts.py
+++ BiomedCLIP_EA_Experiments/camelyon17/ensemble_prompts.py
@@ -48,21 +48,21 @@ def main():
     # )
 
     # save crowded prompts
-    crowded_prompts_filename = "experiment_results/Experiment-67-crowded_prompts.txt"
-    with open(crowded_prompts_filename, 'w') as f:
-        for (neg_prompt, pos_prompt), score in pq.get_best_n(len(pq)):
-            f.write(f"('{neg_prompt}', '{pos_prompt}'), Score: {score}\n")
+    # crowded_prompts_filename = "experiment_results/Experiment-67-crowded_prompts.txt"
+    # with open(crowded_prompts_filename, 'w') as f:
+    #     for (neg_prompt, pos_prompt), score in pq.get_best_n(len(pq)):
+    #         f.write(f"('{neg_prompt}', '{pos_prompt}'), Score: {score}\n")
 
-    # knee point analysis
-    all_current_scores = [score for _, score in pq.get_best_n(
-        len(pq))]  # Get all, sorted by score
-    knee_analyzer = util.KneePointAnalysis(all_current_scores)
-    recommended_n = knee_analyzer.find_knee_point()
+    # # knee point analysis
+    # all_current_scores = [score for _, score in pq.get_best_n(
+    #     len(pq))]  # Get all, sorted by score
+    # knee_analyzer = util.KneePointAnalysis(all_current_scores)
+    # recommended_n = knee_analyzer.find_knee_point()
 
-    print(
-        f"Recommended number of prompts after knee analysis: {recommended_n}")
+    # print(
+    #     f"Recommended number of prompts after knee analysis: {recommended_n}")
 
-    prompts_population = pq.get_best_n(recommended_n)
+    prompts_population = pq.get_best_n(len(pq))
     print(f"Using {len(prompts_population)} prompts for evaluation.")
 
     # Run the evaluation
