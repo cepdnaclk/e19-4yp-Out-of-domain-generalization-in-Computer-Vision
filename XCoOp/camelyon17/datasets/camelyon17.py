385fc23529bf7a12f76bc401ca3b2b6e43a10c0b
Mansitha <iammansitha@gmail.com>
2025-09-06 18:32:20 +0530

    camelyon dataset config

---
+++ XCoOp/camelyon17/datasets/camelyon17.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+from dassl.data.datasets import DATASET_REGISTRY, Datum, DatasetBase
+
+@DATASET_REGISTRY.register()
+class Camelyon17(DatasetBase):
+    dataset_dir = "camelyon17"
+
+    def __init__(self, cfg):
+        root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
+        self.metadata_path = os.path.join(root, "metadata.csv")
+        self.root_dir = os.path.join(root, "images")
+        self.all_class_names = ["non-tumor", "tumor"]
+
+        train = self.read_data(split="train")
+        val = self.read_data(split="val")
+        test = self.read_data(split="test")
+
+        super().__init__(train_x=train, val=val, test=test)
+
+    def read_data(self, split):
+        items = []
+        metadata = pd.read_csv(self.metadata_path)
+        if split == "train":
+            metadata = metadata[(metadata["center"].isin([0, 1, 2])) & (metadata["split"] == 0)]
+        elif split == "val":
+            metadata = metadata[metadata["center"] == 3]
+        elif split == "test":
+            metadata = metadata[metadata["center"] == 4]
+        for _, row in metadata.iterrows():
+            patient = f"{int(row['patient']):03d}"
+            node = int(row['node'])
+            x_coord = int(row['x_coord'])
+            y_coord = int(row['y_coord'])
+            label = int(row['tumor'])
+            img_name = f"patch_patient_{patient}_node_{node}_x_{x_coord}_y_{y_coord}.png"
+            img_path = os.path.join(self.root_dir, f"patient_{patient}_node_{node}", img_name)
+            if os.path.exists(img_path):
+                cls_name = self.all_class_names[label]
+                item = Datum(impath=img_path, label=label, classname=cls_name)
+                items.append(item)
+        return items
