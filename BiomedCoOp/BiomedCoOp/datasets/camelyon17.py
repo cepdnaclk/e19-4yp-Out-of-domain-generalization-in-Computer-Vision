0a91c1527327edbeaba494385568989680b269d4
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2025-09-14 14:24:33 +0530

    Added missing code for few shot training

--- BiomedCoOp/BiomedCoOp/datasets/camelyon17.py
+++ BiomedCoOp/BiomedCoOp/datasets/camelyon17.py
@@ -57,3 +57,30 @@ class Camelyon17Custom(DatasetBase):
             val_few.extend(sampled_items)  # same items for validation
 
         return train_few, val_few
+
+    def read_data(self, split):
+        items = []
+        metadata = pd.read_csv(self.metadata_path)
+        if split == "train":
+            metadata = metadata[(metadata["center"].isin([0, 1, 2])) & (metadata["split"] == 0)]
+        elif split == "val":
+            metadata = metadata[metadata["center"] == 3]
+        elif split == "test":
+            metadata = metadata[metadata["center"] == 4]
+        for _, row in metadata.iterrows():
+            patient = f"{int(row['patient']):03d}"
+            node = int(row['node'])
+            x_coord = int(row['x_coord'])
+            y_coord = int(row['y_coord'])
+            label = int(row['tumor'])
+            # print(f"label is this {label}")
+            img_name = f"patch_patient_{patient}_node_{node}_x_{x_coord}_y_{y_coord}.png"
+            img_path = os.path.join(self.root_dir, f"patches/patient_{patient}_node_{node}", img_name)
+            # print(f"Hi I am the img path {img_path}")
+            if os.path.exists(img_path):
+                # print("Hii I am here.")
+                cls_name = self.all_class_names[label]
+                item = Datum(impath=img_path, label=label, classname=cls_name)
+                items.append(item)
+                # print(f"Item {item}")
+        return items
