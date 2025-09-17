ee220fbb08d6c9c1e7f2f0f90c1fa86360e2c502
Mansitha <iammansitha@gmail.com>
2025-09-17 21:44:06 +0530

    xcoop2shot

---
+++ XCoOp_Org/train_script_camelyon2shot.sh
@@ -0,0 +1,18 @@
+# training script: an example
+DATASET=camelyon17
+ROOT=/storage/projects3/e19-fyp-out-of-domain-gen-in-cv/camelyon17WILDS/
+SHOTS=2
+OUTPUT_DIR=./output_${DATASET}_shot${SHOTS}
+if [ ${DATASET} == "camelyon17" ];
+then
+python train.py \
+ --config-file configs/XCoOp/vit_b16_c4_batch32_camelyon17.yaml \
+ --dataset-config-file configs/datasets/camelyon17.yaml \
+ --trainer XCoOp \
+ --root ${ROOT} \
+ --output-dir ${OUTPUT_DIR} \
+ --seed 3407 \
+ --resume false \
+ --shots ${SHOTS}
+fi
+
