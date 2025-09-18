51de24742e53524953197eccf5a639b1621a19ad
Mansitha <iammansitha@gmail.com>
2025-09-18 08:35:58 +0530

    derm7pt training script

---
+++ XCoOp_Org/train_script_derm7pt2shot.sh
@@ -0,0 +1,16 @@
+# training script: an example
+DATASET=derm7pt
+ROOT=/storage/projects3/e19-fyp-out-of-domain-gen-in-cv/Derm7pt/release_v0/
+SHOTS=2
+OUTPUT_DIR=./output_${DATASET}_shot${SHOTS}
+
+python train.py \
+ --config-file configs/XCoOp/vit_b16_c4_batch32_derm7pt.yaml \
+ --dataset-config-file configs/datasets/derm7pt.yaml \
+ --trainer XCoOp \
+ --root ${ROOT} \
+ --output-dir ${OUTPUT_DIR} \
+ --seed 3407 \
+ --resume false \
+ --shots ${SHOTS}
+
