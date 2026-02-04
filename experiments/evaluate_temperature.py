503115da92c5f6583e2910ba1c3d1b9da65c41ee
Kaushitha Silva <kaushithamsilva@gmail.com>
2026-02-04 15:04:06 +0530

    fix: expand temperature options in evaluate function for more granular testing

--- experiments/evaluate_temperature.py
+++ experiments/evaluate_temperature.py
@@ -98,7 +98,7 @@ def evaluate(
         exists=True,
     ),
     temperatures: list[float] = typer.Option(
-        [1.0, 0.5, 0.1, 0.05],
+        [2.0, 1.0, 0.75, 0.5, 0.25, 0.1, 0.05, 0.01],
         "--temperatures",
         "-T",
         help="Temperature values to test",
